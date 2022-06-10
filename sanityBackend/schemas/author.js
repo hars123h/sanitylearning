9ef9c2fbd3c65b30910a9224f18b28872239dbcc
developerharsh <tripathihk2014@gmail.com>
2022-06-10 22:15:56 +0530

    basic sanity and nextJs setup

---
+++ sanityBackend/schemas/author.js
@@ -0,0 +1,29 @@
+export default {
+    name: 'author',
+    type: 'document',
+      title: 'Author',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'about',
+        type: 'string',
+        title: 'About'
+      },
+      
+      {
+        title: 'Image',
+        type: 'image',
+        name: 'image'
+      },
+
+     
+
+      
+
+      
+    ]
+  }
